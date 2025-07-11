5c271ae2af032ec60a5129eff0caea0220740811
Jeremy Boetticher <me4jeremyboett@gmail.com>
2025-07-10 20:01:20 -0500

    feat: some better spacing things

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,29 @@ function App() {
 
   return (
     <main>
-      <div className='mt-30' />
-      <h1 className="sticky top-8">BOBAMABOB</h1>
-      <h3 className="mt-8">
-        A boba milk tea used to be $2.50. Now we pay up to $8. What happened?
+
+      <section className='h-screen'>
+        <div className="flex justify-center items-center m-10">
+          <img src="/boba machine man.png" style={{ height: '20rem' }} />
+        </div>
+        <div className='mt-20' />
+        <h1 className="sticky top-8">BOBAMABOB</h1>
+        <h3 className="mt-8">
+          A boba milk tea used to be $2.50. Now we pay up to $8. What happened?
+        </h3>
+        <div className="flex flex-col items-center my-32">
+          <span className="text-3xl font-bold animate-bounce mb-4">↓</span>
+          <p className="text-xl text-gray-600">scroll down, it's one of those kinds of websites</p>
+        </div>
+      </section>
+
+      <h3 className="mt-30">
+        Fortunately, us two dudes are working on the bobamabob. A cheap robot that makes cheap boba milk tea.
       </h3>
-
-      <div className="flex justify-center items-center m-20">
-        <img src="/boba machine man.png" className="" style={{ height: '30rem' }} />
-      </div>
-
-      <ScrollPageSection className="mt-12">
-        <h2 className="text-3xl font-bold mb-4">The Boba Machine</h2>
-      </ScrollPageSection>
-      <ScrollPageSection className="mt-12">
-        <h2 className="text-3xl font-bold mb-4">The Boba Machine</h2>
-      </ScrollPageSection>
-      <ScrollPageSection className="mt-12">
-        <h2 className="text-3xl font-bold mb-4">The Boba Machine</h2>
-      </ScrollPageSection>
+      <h3>
+        It's going to be as <b style={{ color: '#8FF0F0' }}>small</b> as a vending machine, and <b style={{ color: '#8FF0F0' }}>cheap</b> enough that it can be placed everywhere.
+      </h3>
+      <footer className='text-grey-400 mt-30'>invest in the bobamabob, and your children will thank you</footer>
     </main>
   )
 }
